dd9255baaffb3cc5afa257eaffac9efbd37c5fac
Fahad Janabi <Fahad.janabi98@yahoo.com>
2022-09-12 12:59:20 -0400

    variables exercise

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string lastName = "Janabi";
+            int myAge = 23;
+            char myChar = '!';
+            Console.WriteLine($"my last name is {lastName } {myAge} {myChar}");
+        }
+    }
+}
